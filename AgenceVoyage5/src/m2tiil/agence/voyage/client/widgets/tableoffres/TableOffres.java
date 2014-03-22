76d3c743d60739a10548e5f988268f42e9ceb218
Gauko <rom.antoin@gmail.fr>
2014-03-22 17:27:44 +0100

    maj

--- AgenceVoyage5/src/m2tiil/agence/voyage/client/widgets/tableoffres/TableOffres.java
+++ AgenceVoyage5/src/m2tiil/agence/voyage/client/widgets/tableoffres/TableOffres.java
@@ -1,8 +1,10 @@
 package m2tiil.agence.voyage.client.widgets.tableoffres;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import m2tiil.agence.voyage.server.bdd.dao.OffreDAO;
+import m2tiil.agence.voyage.client.GreetingService;
+import m2tiil.agence.voyage.client.GreetingServiceAsync;
 import m2tiil.agence.voyage.shared.bdd.pojo.Offre;
 
 import com.google.gwt.cell.client.CheckboxCell;
@@ -13,6 +15,7 @@ import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.DefaultSelectionEventManager;
@@ -25,6 +28,9 @@ public class TableOffres extends Composite {
 
 	final SimplePager pager = new SimplePager();
 	final ListDataProvider<Offre> listOffers = new ListDataProvider<Offre>();
+	
+	private final GreetingServiceAsync service = GWT
+			.create(GreetingService.class);
 
 	@UiField(provided = true)
 	CellTable<Offre> cellTable = new CellTable<Offre>();
@@ -39,7 +45,6 @@ public class TableOffres extends Composite {
 
 	public void createOffersTable() {
 
-		// Create tweet column.
 		TextColumn<Offre> titleCollumn = new TextColumn<Offre>() {
 			@Override
 			public String getValue(Offre offre) {
@@ -47,7 +52,6 @@ public class TableOffres extends Composite {
 			}
 		};
 
-		// Create tweet column.
 		TextColumn<Offre> priceColumn = new TextColumn<Offre>() {
 			@Override
 			public String getValue(Offre offre) {
@@ -72,8 +76,21 @@ public class TableOffres extends Composite {
 		cellTable.addColumn(checkColumn);
 		cellTable.addColumn(titleCollumn, "Intitulle");
 		cellTable.addColumn(priceColumn, "Prix");
-//		ArrayList<Offre> tmp = OffreDAO.selectAll();
-		//listOffers.setList(tmp);
+		service.getOffreDuJour("", new AsyncCallback<List<Offre>>(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+				System.err.println("erreur set list : " + caught);
+				caught.printStackTrace();
+			}
+
+
+			@Override
+			public void onSuccess(List<Offre> result) {
+				listOffers.setList(result);
+			}
+			
+		});
 		listOffers.addDataDisplay(cellTable);
 
 		final SimplePager pager = new SimplePager();
@@ -81,3 +98,4 @@ public class TableOffres extends Composite {
 	}
 
 }
+
