49a394d7a2607c9d1a5aac22789fcc808cddcbc0
Gauko <rom.antoin@gmail.fr>
2014-03-25 21:41:07 +0100

    Modif de la methode onLoad

--- AgenceVoyage5/src/m2tiil/agence/voyage/client/AgenceVoyage5.java
+++ AgenceVoyage5/src/m2tiil/agence/voyage/client/AgenceVoyage5.java
@@ -1,23 +1,12 @@
 package m2tiil.agence.voyage.client;
 
 import m2tiil.agence.voyage.client.widgets.connection.Connection;
+import m2tiil.agence.voyage.client.widgets.research.Research;
 import m2tiil.agence.voyage.client.widgets.tableoffres.TableOffres;
-import m2tiil.agence.voyage.shared.FieldVerifier;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -33,7 +22,8 @@ public class AgenceVoyage5 implements EntryPoint {
 			+ "connection and try again.";
 
 	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
 	 */
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
@@ -45,6 +35,7 @@ public class AgenceVoyage5 implements EntryPoint {
 		VerticalPanel vpanel = new VerticalPanel();
 		vpanel.add(new Connection());
 		vpanel.add(new TableOffres());
+		vpanel.add(new Research());
 		RootPanel.get("page1").add(vpanel);
 	}
 }
